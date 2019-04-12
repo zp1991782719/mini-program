3a52abaa5084ad9bbe48bed5235c4761bc268d5d
zhoupeng <1991782719@qq.com>
2019-04-12 17:43:34 +0800

    mini-app

---
+++ readmecp.md
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    new page
+  </div>
+</template>
+
+<script>
+export default {
+  name:'',
+
+  data() {
+    return {
+      
+    }
+  },
+
+  methods: {
+    
+  }
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/index/index.vue
@@ -0,0 +1,89 @@
+<template>
+  <div>
+    <h1>this is my first mini-app</h1>
+    <ZP />
+  </div>
+</template>
+
+<script>
+
+import ZP from '@/components/zp.vue'
+
+export default {
+  components: { 
+    ZP 
+  },
+
+  data () {
+    return {
+      
+    }
+  },
+
+  methods: {
+    
+  },
+
+  created () {
+    // let app = getApp()
+    wx.showToast({
+      title: ZP.name,
+      icon: 'success',
+      duration: 2000
+    })
+  },
+}
+</script>
+
+<style scoped>
+.userinfo {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.userinfo-avatar {
+  width: 128rpx;
+  height: 128rpx;
+  margin: 20rpx;
+  border-radius: 50%;
+}
+
+.userinfo-nickname {
+  color: #aaa;
+}
+
+.usermotto {
+  margin-top: 150px;
+}
+
+.form-control {
+  display: block;
+  padding: 0 12px;
+  margin-bottom: 5px;
+  border: 1px solid #ccc;
+}
+.all{
+  width:7.5rem;
+  height:1rem;
+  background-color:blue;
+}
+.all:after{
+  display:block;
+  content:'';
+  clear:both;
+}
+.left{
+  float:left;
+  width:3rem;
+  height:1rem;
+  background-color:red;
+}
+
+.right{
+  float:left;
+  width:4.5rem;
+  height:1rem;
+  background-color:green;
+}
+</style>
